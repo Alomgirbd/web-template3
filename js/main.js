e5718d7ed3fee640256ecf8b389baa845f510cb7
Alomgirbd <alomgir56055266@gmail.com>
2020-03-01 15:39:07 +0600

    first commit

---
+++ js/main.js
@@ -0,0 +1,48 @@
+// slick nav
+$(document).ready(function(){
+    $('#menu').slicknav({
+        'label' : '',
+        'appendTo': 'nav' 
+    });
+  });
+
+
+// Pie chart
+$(function() {
+    $('.chart1').easyPieChart({
+        //your options goes here
+        lineWidth:15,
+        size:180,
+        animate:2000,
+        trackColor:"#dfe8ed",
+        barColor:"#30bae7",
+        lineCap:'circle'
+    });
+    $('.chart2').easyPieChart({
+        //your options goes here
+        lineWidth:15,
+        size:180,
+        animate:2000,
+        trackColor:"#dfe8ed",
+        barColor:"#d74680",
+        lineCap:'circle'
+    });
+    $('.chart3').easyPieChart({
+        //your options goes here
+        lineWidth:15,
+        size:180,
+        animate:2000,
+        trackColor:"#dfe8ed",
+        barColor:"#15c7a8",
+        lineCap:'circle'
+    });
+    $('.chart4').easyPieChart({
+        //your options goes here
+        lineWidth:15,
+        size:180,
+        animate:2000,
+        trackColor:"#dfe8ed",
+        barColor:"#eb7d4b",
+        lineCap:'circle'
+    });
+});
